903d8056a128e68ca626442d9b0102a6d5b145ed
Zhengyang Lu <zhengyang.lu@gmail.com>
2015-10-06 22:27:53 -0400

    manacher - longest palindrome

---
+++ manacher.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+void findBMstr(string& str){
+	int *p = new int[str.size()+1];
+	// memset(p, 0, sizeof(p));
+
+	int mx = 0;
+	int pivot = 0;
+
+	for (int i = 1; i < str.size(); i++)
+	{
+		if (i < mx)
+		{
+			p[i] = (p[pivot-(i-pivot)] < (mx-i) ? p[pivot-(i-pivot)] : (mx-i));
+		}
+		else{
+			p[i] = 1;
+		}
+
+		while(str[i-p[i]] == str[i+p[i]]){
+			p[i]++;
+		}
+
+		if (i+p[i] > mx)
+		{
+			mx = i + p[i];
+			pivot = i;
+		}
+	}
+
+	int max = 0;
+	int maxPivot;	// 最大的p[i]对应的pivot
+
+	for (int i = 1; i < str.size(); i++)
+	{	//找出最大的p[i]
+		if (p[i] > max)
+		{
+			maxPivot = i;
+			max = p[i];
+		}
+	}
+	max--; // 因为p[i]初始化为1
+
+	int start = maxPivot - max;
+	int end = maxPivot + max;
+
+	for (int i = start; i <= end; i++)
+	{
+		if (str[i] != '#')
+		{
+			cout << str[i];
+		}
+	}
+
+	cout << endl;
+
+	delete p;
+}
+
+int main(int argc, char const *argv[])
+{
+	string str = "abcdcabcbacda";
+	string str0;
+	str0 += "^#";
+	for (int i = 0; i < str.size(); ++i)
+	{
+		str0 += str[i];
+		str0 += "#";
+	}
+	str0 += "$";
+
+	cout<< str0 << endl;
+	findBMstr(str0);
+	return 0;
+}
